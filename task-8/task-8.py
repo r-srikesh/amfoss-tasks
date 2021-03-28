49bbabdb7397b66ad9847d2b6cc651f8b8578d66
Srikesh Ravikumar <73290918+r-srikesh@users.noreply.github.com>
2021-03-28 15:41:00 +0530

    Update task-8.py

--- task-8/task-8.py
+++ task-8/task-8.py
@@ -1,1 +1,7 @@
-
+import os
+from github import Github   
+user=Github().get_user("amfoss")   
+for repo in user.get_repos():   
+    print(repo.name)
+    os.system('perceval git --json-line https://github.com/amfoss/'+repo.name+'>> task-8.json') 
+os.system('cat task-8.json')
